1a6cd3b4dc1bcdb32dedb71042383055e1517bcd
Claire Jensen <clairej735@gmail.com>
2020-09-21 11:02:09 -0400

    initial edits

--- Labs/03-BouncingDisks/Pair.java
+++ Labs/03-BouncingDisks/Pair.java
@@ -1,3 +1,31 @@
 class Pair {
     // IMPLEMENT THIS CLASS
+
+    public Pair(double something1, double something2) {
+        
+    }
+
+    // public double times(double time) {
+
+    // }
+
+    // public Pair add(double num) {
+
+    // }
+
+    // public int x(int x) {
+
+    // }
+
+    // public int y(int y) {
+
+    // }
+
+    // public int getX(){
+
+    // }
+
+    // public int getY() {
+
+    // }
 }
