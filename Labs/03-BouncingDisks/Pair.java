251d6615a15bf3a372552a0747cabb20fd667d4e
Claire Jensen <clairej735@gmail.com>
2020-09-22 12:34:28 -0400

    added some variables and methods

--- Labs/03-BouncingDisks/Pair.java
+++ Labs/03-BouncingDisks/Pair.java
@@ -1,17 +1,26 @@
 class Pair {
-    // IMPLEMENT THIS CLASS
+    public int y;
+    public int x;
 
     public Pair(double something1, double something2) {
-        
+        this.something1 = something1;
+        this.something2 = something2;
     }
 
-    // public double times(double time) {
+    public double times(double num) {
+        double product;
+        product = this.num * num;
 
-    // }
 
-    // public Pair add(double num) {
+        return product;
+    }
 
-    // }
+    public Pair add(double num) {
+        double sum;
+        sum = this.num + num;
+
+        return sum;
+    }
 
     // public int x(int x) {
 
