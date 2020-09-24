42017b1887f94b30488c8e0bf5dbfe0a18891d31
Claire Jensen <clairej735@gmail.com>
2020-09-24 14:27:35 -0400

    added comments and speed change

--- Labs/03-BouncingDisks/Pair.java
+++ Labs/03-BouncingDisks/Pair.java
@@ -3,14 +3,14 @@ class Pair {
     public double x;
 
     public Pair(double x, double y) {
-        this.x = x;
+        this.x = x; // constructor for the pair class
         this.y = y;
     }
 
     public Pair times(double num) {
         Pair product = new Pair(x, y);
 
-        product.x = this.x * num;
+        product.x = this.x * num; // multiplying vector pairs
         product.y = this.y * num;
 
         return product;
@@ -19,17 +19,19 @@ class Pair {
     public Pair add(Pair num) {
         Pair sum = new Pair(x, y);
 
-        sum.x = this.x + num.x;
+        sum.x = this.x + num.x; // adding vector pairs
         sum.y = this.y + num.y;
 
         return sum;
     }
 
     public void flipX() {
-        x = x * -1;
+        x = x * -1.3; // disc bounces off horizontal wall, changes directions
+                      // and gains 1.3x speed
     }
 
     public void flipY() {
-        y = y * -1;
+        y = y * -1.3;// disc bounces off horizontal wall, changes directions
+                    // and gains 1.3x speed
     }
 }
