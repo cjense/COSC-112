d8c58b09b1a65727e81cdfd167bad554b997b188
Claire Jensen <clairej735@gmail.com>
2020-09-24 13:29:08 -0400

    done with implementation

--- Labs/03-BouncingDisks/Pair.java
+++ Labs/03-BouncingDisks/Pair.java
@@ -1,40 +1,35 @@
 class Pair {
-    public int y;
-    public int x;
+    public double y;
+    public double x;
 
-    public Pair(double something1, double something2) {
-        this.something1 = something1;
-        this.something2 = something2;
+    public Pair(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public double times(double num) {
-        double product;
-        product = this.num * num;
+    public Pair times(double num) {
+        Pair product = new Pair(x, y);
 
+        product.x = this.x * num;
+        product.y = this.y * num;
 
         return product;
     }
 
-    public Pair add(double num) {
-        double sum;
-        sum = this.num + num;
+    public Pair add(Pair num) {
+        Pair sum = new Pair(x, y);
+
+        sum.x = this.x + num.x;
+        sum.y = this.y + num.y;
 
         return sum;
     }
 
-    // public int x(int x) {
+    public void flipX() {
+        x = x * -1;
+    }
 
-    // }
-
-    // public int y(int y) {
-
-    // }
-
-    // public int getX(){
-
-    // }
-
-    // public int getY() {
-
-    // }
+    public void flipY() {
+        y = y * -1;
+    }
 }
