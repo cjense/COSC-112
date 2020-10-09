a2487d760655631484fcf0c16a8d69159147fd6f
Claire Jensen <clairej735@gmail.com>
2020-10-08 20:02:05 -0400

    done

--- Labs/04-Exceptions/Parser.java
+++ Labs/04-Exceptions/Parser.java
@@ -27,5 +27,35 @@ public class Parser {
 
     // Complete the following method as described in the assignment
     public int readInt () {
+        int data = in.read();
+        int count = 0;
+        int number = 0;
+        int digit = data - (int) '0';
+
+        boolean isNegative = false;
+
+        while(data != -1 || in.read() == 32) {
+            if(count == 1 && in.read() == 45) { // if first digit is a hyphen, mark as negative number
+                isNegative = true;
+            }
+            if(count == 1) { // if you're reading the first digit
+                if(in.read() >= 48 && in.read() <= 57) { // if inputStream returns ASCII values 0-9
+                    number += digit;
+                }
+            } else if(count > 1) { 
+                if(in.read() >= 48 && in.read() <= 57) {
+                    number = number * 10 + digit;
+                }
+            }
+            count++;
+
+            if(isNegative) {
+                return -number;
+            } else {
+                return number;
+            }
+
+        }
+        return number;
     }
 }
