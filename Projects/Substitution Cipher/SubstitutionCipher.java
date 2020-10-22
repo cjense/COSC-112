6bf9f76fcb1cb5e8b10069e585afc3a99ecf5dd0
Claire Jensen <clairej735@gmail.com>
2020-10-22 16:49:58 -0400

    initial commit

---
+++ Projects/Substitution Cipher/SubstitutionCipher.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.util.ArrayList;
+
+public class SubstitutionCipher extends Cipher {
+    private long _key;
+
+    public SubstitutionCipher(long key) {
+        _key = key;
+    }
+
+    public ArrayList<Character> encrypt(ArrayList<Character> cleartext) {
+
+    }
+    
+    public ArrayList<Character> decrypt(ArrayList<Character> ciphertext) {
+
+    }
+}
