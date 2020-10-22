747c8e981294ab663730556fb8c474b6a7073d36
Claire Jensen <clairej735@gmail.com>
2020-10-22 18:10:04 -0400

    edits ;

--- Projects/Substitution Cipher/SubstitutionCipher.java
+++ Projects/Substitution Cipher/SubstitutionCipher.java
@@ -3,16 +3,34 @@ import java.util.ArrayList;
 
 public class SubstitutionCipher extends Cipher {
     private long _key;
+    int sub;
+    Random rand = new Random();
+
+    String abc = "abcdefghijklmnopqrstuvwxyz";
+    ArrayList<Character> alphabetray;
+    for(int i = 0; i < abc.length(); i++) {
+        alphabetray.add(abc.charAt(i));
+    }
 
     public SubstitutionCipher(long key) {
         _key = key;
     }
 
     public ArrayList<Character> encrypt(ArrayList<Character> cleartext) {
+        ArrayList<Character> ciphertext;
+        ArrayList<Character> scrambled;
 
+        for(int i = 0; i < cleartext.size() - 1) {
+            sub = rand.nextInt(cleartext.size() - 1);
+            if(scrambled.contains(sub))
+        }
+
+        return ciphertext;
     }
     
     public ArrayList<Character> decrypt(ArrayList<Character> ciphertext) {
+        ArrayList<Character> cleartext;
 
+        return cleartext;
     }
 }
